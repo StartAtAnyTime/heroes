9323b8a6003aace8e38c61cfd4417d04dd7ef850
kangdonghui1 <kdh_1029@163.com>
2018-08-07 21:03:13 +0800

    添加英雄完成

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,6 +6,8 @@ Vue.use(VueRouter)
 import HeroesList from '../views/heroes/list.vue';
 import EquipsList from '../views/equips/list.vue';
 import weaponsList from '../views/weapons/list.vue';
+// 添加英雄组件
+import HeroesAdd from '../views/heroes/add.vue'
 
 
 
@@ -13,9 +15,11 @@ import weaponsList from '../views/weapons/list.vue';
 
 
 var router = new VueRouter({
+    linkActiveClass:'active',
     routes:[
         { path:'/', redirect:'/heroes'},
         {name:'heroes', path:'/heroes', component:HeroesList},
+        {name:'heroesAdd', path:'/heroes/add', component:HeroesAdd},
         {name:'equips', path:'/equips', component:EquipsList},
         {name:'weapons', path:'/weapons', component:weaponsList}
     ]
---
+++ src/views/heroes/add.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h2 class="sub-header">添加英雄</h2>
+        <form>
+            <div class="form-group">
+            <label for="name">英雄名称</label>
+            <input type="text" class="form-control" id="name" placeholder="英雄名称" v-model="formdata.name">
+            </div>
+            <div class="form-group">
+            <label for="gender">英雄性别</label>
+            <input type="text" class="form-control" id="gender" placeholder="英雄性别" v-model="formdata.gender">
+            </div>
+            
+            <button @click.prevent="handleAdd" type="submit" class="btn btn-success">提交</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            formdata:{
+                name:'',
+                gender:''
+            }
+        }
+    },
+    methods:{
+        handleAdd(){
+            axios
+                .post('http://localhost:3000/heroes',this.formdata)
+                .then(res => {
+                    if(res.status===201){
+                        this.$router.push('/heroes')
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                    
+                })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
